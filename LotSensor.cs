e235a36649ffc492c86419b23532478546736762
Austin Lindler <austinlindler@yahoo.com>
2023-06-06 17:15:25 -0500

    Building classes ParkingMeter, Customer, LotSensor, EventLog

---
+++ LotSensor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingAssistant_V._1
+{
+    internal class LotSensor
+    {
+    }
+}
