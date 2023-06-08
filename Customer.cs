ba53e69a0433483bce36c8449ca81497102845e5
Austin Lindler <austinlindler@yahoo.com>
2023-06-08 16:28:59 -0500

    Building out class for customer

--- Customer.cs
+++ Customer.cs
@@ -6,11 +6,39 @@ using System.Threading.Tasks;
 
 namespace ParkingAssistant_V._1
 {
+    [Serializable]
     internal class Customer
     {
-        /*
-         * This is a test from Charlse
-         * 
-         */
+        //Attributes
+
+        protected string plateNumber;
+        protected string cardNumber;
+        protected string firstName;
+        protected string lastName;
+        protected DateTime timeIn;
+        protected DateTime timeOut;
+
+        public Customer()
+        {
+            plateNumber = "unknown";
+            cardNumber = "unknown";
+            firstName = "unknown";
+            lastName = "unknown";
+            timeIn = DateTime.MinValue;
+            timeOut = DateTime.MinValue;
+
+        }
+        //constructors
+        public Customer (string plateNumber, string cardNumber, string firstName, string lastName, DateTime timeIn, DateTime timeOut)
+        {
+            this.plateNumber = plateNumber;
+            this.cardNumber = cardNumber;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.timeIn = timeIn;
+            this.timeOut = timeOut;
+        }
     }
+
+
 }
