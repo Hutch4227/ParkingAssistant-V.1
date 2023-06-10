0e5b7e6d4705687cc7289ded8e5ea6d03fabf0ce
Charles Byrd <charles.byrd380@gmail.com>
2023-06-10 15:51:35 -0500

    Added Image to main Menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,28 +29,47 @@
         private void InitializeComponent()
         {
             this.btnAddVehicle = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAddVehicle
             // 
             this.btnAddVehicle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddVehicle.Location = new System.Drawing.Point(308, 569);
+            this.btnAddVehicle.Location = new System.Drawing.Point(254, 557);
+            this.btnAddVehicle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAddVehicle.Name = "btnAddVehicle";
-            this.btnAddVehicle.Size = new System.Drawing.Size(129, 51);
+            this.btnAddVehicle.Size = new System.Drawing.Size(97, 41);
             this.btnAddVehicle.TabIndex = 0;
             this.btnAddVehicle.Text = "Add Vehicle";
             this.btnAddVehicle.UseVisualStyleBackColor = true;
             this.btnAddVehicle.Click += new System.EventHandler(this.BtnAddVehicle_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::ParkingAssistant_V._1.Properties.Resources.AppLog;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(599, 526);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(828, 775);
+            this.ClientSize = new System.Drawing.Size(621, 630);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnAddVehicle);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Parking Assistant";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +77,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnAddVehicle;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
