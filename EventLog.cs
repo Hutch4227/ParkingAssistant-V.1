2bd6db419d6b9ebf4e5fe102357f9623e58ce0a3
Charles Byrd <charles.byrd380@gmail.com>
2023-06-10 21:23:21 -0500

    Testing App

--- EventLog.cs
+++ EventLog.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Configuration;
+using System.Windows.Forms;
 
 namespace ParkingAssistant_V._1
 {
@@ -13,12 +14,30 @@ namespace ParkingAssistant_V._1
         // Capture events for the parking system.
         // Display dates and times of each parking event.
 
+       
+        public static void CirCretor()
+        {
+            string V = ConfigurationManager.AppSettings["EventLogPath"];
+
+            if (Directory.Exists(V) == false)
+            {
+                Directory.CreateDirectory(V);
+                MessageBox.Show("Dir has been created");
+            }
+            else
+            {
+                MessageBox.Show("The Path is already there.......");
+            }
+
+        }
+
         public static void Writelog(string message)
         {
             //Setting the path to save to file to
-            string EventlogPath = ConfigurationManager.AppSettings["EventLogPath"];
 
-            using (StreamWriter sw = new StreamWriter(EventlogPath, true))
+            string logPath = ConfigurationManager.AppSettings["EventLogPath"];
+
+            using (StreamWriter sw = new StreamWriter(logPath, true))
             {
                 sw.WriteLine($"*************************************************************");
                 sw.WriteLine($"{DateTime.Now} : {message}");
